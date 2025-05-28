fc60156975cf9b319c5db5f3d874708ec8babfb7
NikoLicht <artmole.niko@gmail.com>
2025-05-28 20:22:40 +0200

    fixed images shown double in rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -42,7 +42,7 @@ export async function GET() {
 items: posts.map((post) => ({
   title: post.data.title,
   pubDate: post.data.pubDate,
-  description: `<img src=${post.data.cover} alt="${post.data.title}" /><p>${post.data.description}</p>`,
+  description: `<p>${post.data.description}</p>`,
   link: `/portfolio/blog/${post.slug}/`,
   customData: `
     <media:content url="${post.data.cover}" medium="image" />
